5645b5343f97201d335e65c88d36a48b74b12ebb
Juliano Metzker <julianometzker87@gmail.com>
2024-01-19 13:40:42 -0300

    animated_boxes, title and subtitle done

--- main.py
+++ main.py
@@ -30,6 +30,64 @@ def main(page: Page):
     page.vertical_alignment = 'center'
     page.bgcolor = '#1f262f'
 
+    # "To animate the boxes, we'll need a constant while loop to keep running."
+    def animate_boxes():
+        # "To animate the boxes, we'll need to create several variables"
+        # "We want one box to go clockwise, and the other counter clock"
+        clock_wise_rotate = pi / 4
+        counter_clock_wise_rotate = -pi * 2
+
+        # "Next we need to get the box instances so we can animate the rotation"
+        # "This long line of code is where the red box is in the control list"
+        # "This can be simplified by passing in the instance to a dict!"
+        red_box = page.controls[0].content.content.controls[1].controls[0].controls[0]
+        blue_box = page.controls[0].content.content.controls[1].controls[1].controls[0]
+
+        # "A counter so we can reverse the rotation direction"
+        counter = 0
+        while True:
+            # "We want to rotate 4x before we switch directions"
+            if counter >= 0 and counter <= 4:
+                red_box.rotate = transform.Rotate(
+                    counter_clock_wise_rotate, alignment.center
+                )
+                blue_box.rotate = transform.Rotate(
+                    clock_wise_rotate, alignment.center
+                )
+
+                red_box.update()
+                blue_box.update()
+
+                clock_wise_rotate += pi / 2
+                counter_clock_wise_rotate -= pi / 2
+                
+                counter += 1
+                time.sleep(0.7)
+
+                # "Now we want the boxes to reverse"
+            if counter >= 5 and counter <=10:
+                # "Make sure to reverese the rotation angles"
+                clock_wise_rotate -= pi / 2
+                counter_clock_wise_rotate += pi / 2
+
+                red_box.rotate = transform.Rotate(
+                    counter_clock_wise_rotate, alignment.center
+                )
+                blue_box.rotate = transform.Rotate(
+                    clock_wise_rotate, alignment.center
+                )
+
+                red_box.update()
+                blue_box.update()
+
+                counter += 1
+                time.sleep(0.7)
+
+
+            # "Finally we reset the counter back to 0 at 10"
+            if counter > 10:
+                counter = 0
+
     # main page controls
     page.add(
         Card(
@@ -43,13 +101,37 @@ def main(page: Page):
                     horizontal_alignment=CrossAxisAlignment.CENTER,
                     controls=[
                         # add main controls here
-                         
-                    ]
-                )
-            )
+                        Divider(height=40, color='transparent'),
+                        Stack(
+                            controls=[
+                                # "We will pass the animated box class in here"
+                                # "recall that the class takes in 3 arguments..."
+                                AnimatedBox("#e9665a", None, 0),
+                                AnimatedBox("#7df6dd", "#23262a", pi / 4),
+                            ]
+                        ),
+                        Divider(height=20, color='transparent'),
+                        # Some text
+                        Column(
+                            alignment=MainAxisAlignment.CENTER,
+                            horizontal_alignment=CrossAxisAlignment.CENTER,
+                            spacing=5,
+                            controls=[
+                                Text("Sign In Below", size=22, weight="bold"),
+                                Text(
+                                    "Advanced Python-Flet UI Concepts",
+                                    size=13,
+                                    weight="bold",
+                                )
+                            ]
+                        ),
+                    ],
+                ),
+            ),
         )
     )
     page.update()
+    animate_boxes()
 
 if __name__ == "__main__":
     flet.app(target=main)
